5fae1b5c54fc0eb1ff10707a36348cd6095e6db1
Daniel Van Kirk <davk0810@gmail.com>
2023-08-03 18:19:23 -0400

    upload3

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,27 @@
 USE employee_db; 
 INSERT INTO roles (title, salary, role_id)
 VALUES
-("lead")
+("sales person", 60000, 1),
+("sales lead", 70000, 1),
+("lead engineer", 70000, 2),
+("software engineer", 60000, 2),
+("accountant", 60000, 3),
+("legal team lead", 75000, 4),
+("lawyer", 55000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+("John", "Doe", 1),
+("Mike", "Chan", 1),
+("Ashley", "Rodriguez", 2),
+("Kevin", "Tupik", 2),
+("Malia", "Brown", 3),
+("Sarah", "Lourd", 4),
+("Tom", "Allen", 4);
+
+INSERT INTO departments (name)
+VALUES
+("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
